270665e8c3e4db0fcc0ab210c8078a56ebdafb34
eanesparrago <eanesparrago@gmail.com>
2021-04-01 20:13:36 +0800

    Add Antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "destyle.css";
+import "antd/dist/antd.css";
+
+import { Button } from "antd";
 
 import theme from "styles/theme";
 import GlobalStyle from "styles/GlobalStyle";
