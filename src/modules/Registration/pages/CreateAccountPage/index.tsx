a0a43fcd54a64786b64c17745c0a84d1a7dcf93b
eanesparrago <eanesparrago@gmail.com>
2021-04-09 21:37:43 +0800

    Feat(Registration): Add checking if email exist in CreateAccountPage

--- src/modules/Registration/pages/CreateAccountPage/index.tsx
+++ src/modules/Registration/pages/CreateAccountPage/index.tsx
@@ -3,9 +3,10 @@ import { useHistory } from "react-router-dom";
 import { useAppDispatch } from "app/hooks";
 import localStorage from "utils/localStorage";
 import { setEmailAddress } from "../../registrationSlice";
-import useSendSignInLinkToEmail from "./useSendSignInLinkToEmail";
+import useSendSignInLinkToEmail from "./utils/useSendSignInLinkToEmail";
+import checkIfEmailExists from "./utils/checkIfEmailExists";
 
-import { Typography, Input, Form, Button } from "antd";
+import { Typography, Input, Form, Button, message } from "antd";
 import Logo from "components/Logo";
 import MainLayout from "../../layouts/MainLayout";
 
@@ -17,6 +18,12 @@ function CreateAccountPage() {
   const { sendSignInLinkToEmail, isLoading } = useSendSignInLinkToEmail();
 
   async function onFinish({ emailAddress }: { emailAddress: string }) {
+    if (await checkIfEmailExists(emailAddress)) {
+      history.push("/login");
+      message.info("Email is already registered");
+      return;
+    }
+
     dispatch(setEmailAddress(emailAddress));
 
     try {
