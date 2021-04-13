d72465b761f5c6f6fdcaedf580e9eec6e810fbd7
eanesparrago <eanesparrago@gmail.com>
2021-04-13 18:43:56 +0800

    Add react-firebase-hooks and useCurrentUser

---
+++ src/api/hooks/useCurrentUser.ts
@@ -0,0 +1,11 @@
+import { firestore, auth } from "api/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useDocument } from "react-firebase-hooks/firestore";
+
+function useCurrentUser() {
+  const [user] = useAuthState(auth);
+
+  return useDocument(user && firestore.doc(`users/${user.uid}`));
+}
+
+export default useCurrentUser;
