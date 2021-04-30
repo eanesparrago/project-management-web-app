35c33f41e6e98e08ea1ee04996d1828b560ad672
eanesparrago <eanesparrago@gmail.com>
2021-04-30 22:46:01 +0800

    Feat(useDeleteGroup): Show confirmation if group contains tasks during delete

--- src/api/groups/useDeleteGroup.ts
+++ src/api/groups/useDeleteGroup.ts
@@ -1,12 +1,34 @@
 import { message } from "antd";
+import confirm from "antd/lib/modal/confirm";
 import { firestore } from "api/firebase";
 
 function useDeleteGroup() {
   async function deleteGroup(projectId: string, groupId: string) {
     try {
-      await firestore.doc(`projects/${projectId}/groups/${groupId}`).delete();
+      const tasksSnapshot = await firestore
+        .collection(`projects/${projectId}/groups/${groupId}/tasks`)
+        .get();
 
-      message.info("Group deleted");
+      const groupRef = firestore.doc(`projects/${projectId}/groups/${groupId}`);
+
+      const handleDeleteGroup = async () => {
+        await groupRef.delete();
+        message.info("Group deleted");
+      };
+
+      if (tasksSnapshot.size > 0) {
+        confirm({
+          title: "Are you sure you want to delete this group?",
+          content: `This group contains ${tasksSnapshot.size} task(s).`,
+          maskClosable: true,
+          okText: "Delete group",
+          okButtonProps: { danger: true },
+          onOk: async () => await handleDeleteGroup(),
+        });
+        return;
+      }
+
+      await handleDeleteGroup();
     } catch (error) {
       console.error(error);
       message.error("Error deleting group");
