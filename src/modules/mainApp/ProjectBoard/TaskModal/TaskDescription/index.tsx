3e47afab2bd0c70ccb903a842d1ba169988cdcd8
eanesparrago <eanesparrago@gmail.com>
2021-04-30 21:34:17 +0800

    Feat(TaskDescription): Add Edit Task Description

--- src/modules/mainApp/ProjectBoard/TaskModal/TaskDescription/index.tsx
+++ src/modules/mainApp/ProjectBoard/TaskModal/TaskDescription/index.tsx
@@ -1,22 +1,43 @@
-import { Typography } from "antd";
+import { ChangeEvent, useState } from "react";
 
-const { Text } = Typography;
+import { Input } from "antd";
+
+import { useParams } from "react-router";
+import { firestore } from "api/firebase";
+
+const { TextArea } = Input;
 
 type TaskDescriptionProps = {
   description?: string;
 };
 
 function TaskDescription({ description }: TaskDescriptionProps) {
+  const [localDescription, setLocalDescription] = useState(description);
+  const { projectId, groupId, taskId } = useParams<{
+    projectId: string;
+    groupId: string;
+    taskId: string;
+  }>();
+
+  function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setLocalDescription(event.target.value);
+  }
+
+  function updateDescription() {
+    firestore
+      .doc(`projects/${projectId}/groups/${groupId}/tasks/${taskId}`)
+      .update({ description: localDescription });
+  }
+
   return (
-    <span>
-      <Text editable>
-        {description ? (
-          description
-        ) : (
-          <Text type="secondary">Add more detail to this task...</Text>
-        )}
-      </Text>
-    </span>
+    <TextArea
+      placeholder="Add more detail to this task..."
+      bordered={false}
+      rows={4}
+      value={localDescription}
+      onChange={handleChange}
+      onBlur={updateDescription}
+    />
   );
 }
 
